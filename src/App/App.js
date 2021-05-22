456d413abe6a436dbb65fcc3763fc4529e50d634
Jim Administrator <jamesdavidconner@gamil.com>
2021-05-22 10:53:59 -0500

    added fb auth code

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import firebase from 'firebase';
+// import 'firebase/auth';
 import './App.scss';
 
 function App() {
-  // const [domWriting, setDomWriting] = useState('Nothing Here!');
+  const [user, setUser] = useState(null);
+  // const [players, setProjects] = useState([]);
 
-  // setDomWriting('test');
-
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((userInState) => {
+      if (userInState) {
+        const userInfoObject = {
+          fullName: userInState.displayName,
+          profileImage: userInState.photoURL,
+          uid: userInState.uid,
+          userName: userInState.email.split('@')[0]
+        };
+        setUser(userInfoObject);
+        // getProjects(userInState.uid).then((playersArray) => setProjects(playersArray));
+      } else if (user || user === null) {
+        setUser(false);
+        // setProjects([]);
+      }
+    });
+  }, []);
   return (
     <div className='App'>
       {console.warn('test')}
