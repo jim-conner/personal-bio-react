aa30d185d26d3d320d0c246fa72b12c18b88fefa
Jim Administrator <jamesdavidconner@gamil.com>
2021-05-21 23:27:44 -0500

    deleting template code, renamed files

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  // const [domWriting, setDomWriting] = useState('Nothing Here!');
+
+  // setDomWriting('test');
+
+  return (
+    <div className='App'>
+      {console.warn('test')}
+      <h3>{'test'}</h3>
+    </div>
+  );
+}
+
+export default App;
