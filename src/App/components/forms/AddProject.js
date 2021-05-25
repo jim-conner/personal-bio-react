e0a6570cdc83b86d18bede9b4c09e70b9e998d92
Jim Administrator <jamesdavidconner@gamil.com>
2021-05-24 20:07:25 -0500

    working on form values and passing projectObj

--- src/App/components/forms/AddProject.js
+++ src/App/components/forms/AddProject.js
@@ -1,57 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import {
   Button, Form, FormGroup, Label, Input
 } from 'reactstrap';
+import { createProject } from '../../../helpers/data/projectData';
 
-function AddEditProjectForm() {
-  // const [project, setProject] = useState({
-  //   title: title || '',
-  //   firebaseKey: firebaseKey || null,
-  //   image: image || '',
-  //   gitHubUrl: gitHubUrl || '',
-  //   netlifyUrl: netlifyUrl || ''
-  // });
-  // console.warn(setProject(project));
-  // const handleInputChange = (e) => {
-  //   setProject((prevState) => ({
-  //     ...prevState,
-  //     [e.target.name]: e.target.value,
-  //   }));
-  // };
+function AddEditProjectForm({ setProjects }) {
+  const [project, setProject] = useState({
+    title: title || '',
+    firebaseKey: firebaseKey || null,
+    image: image || '',
+    gitHubUrl: gitHubUrl || '',
+    netlifyUrl: netlifyUrl || ''
+  });
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
+  const handleInputChange = (e) => {
+    setProject((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-  // };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createProject(project).then((projectsArray) => (setProjects(projectsArray)));
+    // console.warn(project);
+  };
 
   return (
   <div>
-    <Form>
+    <Form autoComplete='off' onSubmit={handleSubmit}>
       <FormGroup>
         <Label for="title">App Title</Label>
         <Input type="text" name="title"
-        placeholder="with a placeholder" />
+        placeholder="with a placeholder"
+        value={project.title}
+        onChange={handleInputChange}
+        />
       </FormGroup>
       <FormGroup>
         <Label for="deployUrl">Deploy Link</Label>
-        <Input type="Url" name="deployedLink"
-        placeholder="Enter Deployed Site URL" />
+        <Input type="Url" name="deployUrl"
+        placeholder="Enter Deployed Site URL"
+        value={project.deployUrl}
+        onChange={handleInputChange}
+        />
       </FormGroup>
       <FormGroup>
         <Label for="gitHubUrl">GitHub Repo Link</Label>
         <Input type="Url" name="gitHubLink"
-        placeholder="Enter GitHub Repo URL"/>
+        placeholder="Enter GitHub Repo URL"
+        value={project.gitHubUrl}
+        onChange={handleInputChange}
+        />
       </FormGroup>
       <FormGroup>
         <Label for="description">Description</Label>
-        <Input type="textarea" name="text" id="exampleText" />
+        <Input type="textarea" name="text"
+        placeholder="Describe the Project"
+        value={project.description}
+        onChange={handleInputChange}
+        />
       </FormGroup>
       <FormGroup tag="fieldset">
-        <legend>Active?</legend>
+        <legend>Available?</legend>
         <FormGroup check>
           <Label check>
-            <Input type="radio" name="radio1" />{' '}
+            <Input type="radio" name="radio1" 
+              // value={project.available} need to work on this later T/F
+            />{' '}
             Yes
           </Label>
         </FormGroup>
@@ -70,7 +87,9 @@ function AddEditProjectForm() {
 }
 
 AddEditProjectForm.propTypes = {
-  projectObj: PropTypes.object
+  projectObj: PropTypes.object,
+  setProjects: PropTypes.func.isRequired
+
 };
 
 export default AddEditProjectForm;
