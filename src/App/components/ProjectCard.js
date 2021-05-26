c20ed8a813bf6406c6ea6e2b1b41965732327920
Jim Administrator <jamesdavidconner@gamil.com>
2021-05-25 21:26:04 -0500

    console warning with pending promise

--- src/App/components/ProjectCard.js
+++ src/App/components/ProjectCard.js
@@ -17,40 +17,25 @@ import { deleteProject } from '../../helpers/data/projectData';
 function ProjectCard({ admin, setProjects, ...projectObj }) {
   // const [editNow, setEditNow] = useState(false);
 
+  // const handleClick = (type) => {
+  //   switch (type) {
+  //     case 'delete':
+  //       deleteProject(projectObj.firebaseKey)
+  //         .then((projectsArray) => setProjects(projectsArray));
+  //       break;
+  //     case 'edit':
+  //       // setEditNow((prevState) => !prevState);
+  //       break;
+  //     default:
+  //       console.warn('nothing selected');
+  //   }
+  // };
   const handleClick = (type) => {
-    switch (type) {
-      case 'delete':
-        // console.warn(projectObj.firebaseKey);
-        // console.warn(deleteProject(projectObj.firebaseKey));
-        deleteProject(projectObj.firebaseKey)
-          .then((projectsArray) => setProjects(projectsArray));
-
-        break;
-      case 'edit':
-        // setEditNow((prevState) => !prevState);
-        break;
-      default:
-        console.warn('nothing selected');
+    if (type === 'delete') {
+      console.warn(projectObj.firebaseKey);
+      console.warn(deleteProject(projectObj.firebaseKey));
     }
   };
-
-  // const adminOnlyButtons = () => (
-  //   <>
-  //     {
-  //       admin !== null
-  //       && <>
-  //         {
-  //           admin === true
-  //             ? <>
-  //               <Button color='danger'onClick={() => handleClick('delete')}>Delete</Button>
-  //               <Button color='success'onClick={() => handleClick('edit')}>Edit</Button>
-  //             </>
-  //             : ''
-  //         }
-  //       </>
-  //     }
-  //     </>
-  // );
 
   return (
       <Card body
@@ -64,20 +49,12 @@ function ProjectCard({ admin, setProjects, ...projectObj }) {
           <CardTitle tag="h3">{projectObj.title}</CardTitle>
           <CardText tag="h5">{projectObj.description}</CardText>
           <CardLink href={projectObj.gitHubUrl}>GitHub Repo</CardLink>
-          {/* <Button color='info' onClick={() => handleClick('edit')}> */}
             {/* {editNow ? 'Close Form' : 'Edit Form'} */}
-          {/* </Button> */}
           {/* <Button to={deployedUrl}>Deployed Site</Button> */}
           {/* {
-            editNow && <PlayerForm change this fr0om player
-            setPlayers={setPlayers}
-            firebaseKey={firebaseKey}
-            uid={uid}
-            user={user}
-            name={name}
-            imageUrl={imageUrl}
-            position={position}
-          />} */}
+            editNow &&
+            AddProject edit for will go here
+           */}
         </CardBody>
         {
             admin
@@ -87,7 +64,6 @@ function ProjectCard({ admin, setProjects, ...projectObj }) {
               </>
               : ''
           }
-         {/* {adminOnlyButtons()} */}
       </Card>
   );
 }
